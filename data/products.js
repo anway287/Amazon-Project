a3377e9a6f49a2cb391fec8da0531ebc8dd09be3
Anway Atkekar <anwayatkekar@gmail.com>
2025-02-20 20:23:15 -0500

    Created OOP version of cart

--- data/products.js
+++ data/products.js
@@ -1,12 +1,13 @@
-export function getProductById(productId) {
+export function getProduct(productId) {
   let matchingProduct;
 
-      products.forEach((product) => {
-          if (product.id === productId) {
-              matchingProduct = product;
-          }
-      });
-      return matchingProduct;
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+
+  return matchingProduct;
 }
 
 export const products = [
